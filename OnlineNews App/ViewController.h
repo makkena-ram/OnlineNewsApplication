10e8e99d73c009848db60785849c3187e953199c
Chalamalla <s518600@chimac20.nwmissouri.edu>
2014-03-10 17:54:12 -0500

    Initial Commit

---
+++ OnlineNews App/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  OnlineNews App
+//
+//  Created by Chalamalla,Sarita on 3/10/14.
+//  Copyright (c) 2014 Student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ OnlineNews AppTests/OnlineNews_AppTests.m
@@ -0,0 +1,34 @@
+//
+//  OnlineNews_AppTests.m
+//  OnlineNews AppTests
+//
+//  Created by Chalamalla,Sarita on 3/10/14.
+//  Copyright (c) 2014 Student. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface OnlineNews_AppTests : XCTestCase
+
+@end
+
+@implementation OnlineNews_AppTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
